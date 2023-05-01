2f8ace6ca3796d77c70e24b0b82c24ef29c3a18a
abhi-j <abhijitpanchal01@gmail.com>
2023-05-02 02:40:59 +0530

    ✅ authentication done

--- api/index.js
+++ api/index.js
@@ -4,11 +4,15 @@ const  mongoose  = require('mongoose')
 const User = require('./models/user/User')
 const app = express()
 require('dotenv').config()
-
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const PORT = process.env.PORT || 3300;
 
-app.use(cors())
+const salt= bcrypt.genSaltSync(10);
+const secret = 'randomSecret'
+
+app.use(cors({credentials:true, origin:'http://127.0.0.1:5173'}))
 app.use(express.json())
 
 
@@ -24,13 +28,28 @@ mongoose.connect(process.env.MONGOOSE_URL)
   app.post('/register', async (req, res) => {
     try {
       const { username, password } = req.body
-      const userDoc = await User.create({ username, password })
+      const userDoc = await User.create({ username, password: bcrypt.hashSync(password, salt) })
       res.json(userDoc)
     } catch (error) {
       console.error(error)
       res.status(500).send('An error occurred while creating the user.')
     }
   })
+
+  app.post('/login', async (req, res) => {
+      const { username, password } = req.body
+      const userDoc = await User.findOne({ username })
+      const passOk = bcrypt.compareSync(password, userDoc.password)
+      if(passOk){
+        //logged in 
+        jwt.sign({username, id: userDoc._id},secret, {}, (err, token)=>{
+          if(err) throw err; 
+          res.cookie('token', token).json('ok');
+        } )
+      }else{
+      res.status(500).json('An error occurred while creating the user.')
+      }
+    })
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}.` )
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,13 +1,46 @@
+import { useState } from 'react'
+import {Navigate} from 'react-router-dom'
 import './Login.scss'
 
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [redirect, setRedirect] = useState('')
+
+  async function login(e){
+    e.preventDefault();
+   const response = await fetch('http://localhost:3300/login', {
+      method: 'POST', 
+      body: JSON.stringify({username, password}),
+      headers: {'content-type': 'application/json'},
+      credentials:'include', 
+    })
+    if(response.ok){
+      setRedirect(true)
+      alert('Successfully logged in')
+    }
+    else{
+      alert('Wrong Credentials' )
+    }
+  }
+
+  if(redirect){
+    return <Navigate to={'/'}/>
+  }
+
   return (
     <div className="login-container">
       <div className='login-card'>
         <h2>Login</h2>  
-        <form action="" className='login-form'>
-          <input className='email' type="email" name='email' placeholder='Enter your email'/>
-          <input className='pwd' type="password" name='password' placeholder='*******'/>
+        <form action="" className='login-form' onSubmit={login}>
+          <input className='email' type="email" name='email' placeholder='Enter your email'
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+          <input className='pwd' type="password" name='password' placeholder='*******'
+            value={password}
+            onChange={e=> setPassword(e.target.value)}
+          />
           <button className='btn-primary'>Login</button>
         </form> 
       </div>
